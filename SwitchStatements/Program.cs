9abb4c16dc4b61b7a28d19b8a035601ee6fa4bc8
Jake Lane <lanejake13@gmail.com>
2020-10-27 15:04:51 -0400

    finished exercise

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string subject = "C#";
+
+            Console.WriteLine("What is your favorite subject?");
+            string userSubject = Console.ReadLine();
+
+            switch (userSubject)
+            {
+                case "c#":
+                    Console.WriteLine("you're in the right class");
+                    break;
+                case "Math":
+                    Console.WriteLine("math is fun but this aint it chief");
+                    break;
+                case "English":
+                    Console.WriteLine("ew english sucks");
+                    break;
+                default:
+                    Console.WriteLine("ya i've never heard of that");
+                    break;
+            }
+        }
+    }
+}
